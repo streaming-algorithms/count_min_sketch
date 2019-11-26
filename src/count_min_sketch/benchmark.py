c5e195c827d181654f87c892cdfeaeaa0db2b1f1
mazghal <aminezghal@vente-privee.com>
2019-11-26 18:05:43 +0100

    add split_number method

--- src/count_min_sketch/benchmark.py
+++ src/count_min_sketch/benchmark.py
@@ -1,4 +1,16 @@
+from random import randint
 
 
 def absolute_error(expected, actual):
     return abs(expected - actual)
+
+
+def split_number(number, max_splits):
+    splits = list()
+    while number > 0 and len(splits) < max_splits - 1:
+        split = randint(1, number)
+        number -= split
+        splits.append(split)
+    if max_splits > 0:
+        splits.append(number)
+    return splits
--- tests/count_min_sketch/test_benchmark.py
+++ tests/count_min_sketch/test_benchmark.py
@@ -1,6 +1,5 @@
 import pytest
-
-from count_min_sketch.benchmark import absolute_error
+from count_min_sketch.benchmark import absolute_error, split_number
 
 
 @pytest.mark.parametrize('expected, actual', [
@@ -10,7 +9,16 @@ from count_min_sketch.benchmark import absolute_error
     (10000, 1)
 ])
 def test_absolute_error(expected, actual):
-    assert absolute_error(
-        expected=expected,
-        actual=actual
-    ) == abs(expected - actual)
+    assert absolute_error(expected=expected, actual=actual) == abs(expected - actual)
+
+
+@pytest.mark.parametrize('number, max_splits', [
+    (100, 10),
+    (50, 3),
+    (10, 10),
+    (42, 31)
+])
+def test_split_number(number, max_splits):
+    splits = split_number(number=number, max_splits=max_splits)
+    assert len(splits) <= max_splits
+    assert sum(splits) == number
