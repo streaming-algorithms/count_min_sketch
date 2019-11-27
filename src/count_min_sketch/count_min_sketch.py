7edc781abfcd38dc74251c5eecdbcf1aa5732f48
mazghal <aminezghal@vente-privee.com>
2019-11-27 15:11:21 +0100

    add logs to from_error class method

--- src/count_min_sketch/count_min_sketch.py
+++ src/count_min_sketch/count_min_sketch.py
@@ -1,3 +1,5 @@
+import logging
+
 import numpy as np
 
 
@@ -9,15 +11,19 @@ class CountMinSketch:
 
     def add_item(self, item, frequency):
         for row in range(self.nb_rows):
-            column = hash(str(row) + item) % self.nb_columns
+            column = hash(str(row) + str(item)) % self.nb_columns
             self.table[row][column] += frequency
 
     def retrieve_item_frequency(self, item):
         return np.min(
-            [self.table[row][hash(str(row) + item) % self.nb_columns] for row in
+            [self.table[row][hash(str(row) + str(item)) % self.nb_columns] for row in
              range(self.nb_rows)])
 
     @classmethod
     def from_error(cls, absolute_error, error_probability):
-        return cls(nb_columns=int(np.ceil(np.exp(1) / absolute_error)),
-                   nb_rows=int(np.ceil(np.log2(1 / error_probability))))
+        logger = logging.getLogger(__name__)
+        nb_columns = int(np.ceil(np.exp(1) / absolute_error))
+        nb_rows = int(np.ceil(np.log2(1 / error_probability)))
+        logger.info(
+            f'Optimal Count-min sketch with {nb_columns} columns and {nb_rows} rows')
+        return cls(nb_columns=nb_columns, nb_rows=nb_rows)
