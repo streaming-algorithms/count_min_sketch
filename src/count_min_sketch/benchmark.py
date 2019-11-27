49fc872a67e03189e7d6b2cd6c02c797eccb7369
mazghal <aminezghal@vente-privee.com>
2019-11-27 15:12:10 +0100

    add mothod to benchmark a count-min sketch performance in terms of absolute error and error probability

--- src/count_min_sketch/benchmark.py
+++ src/count_min_sketch/benchmark.py
@@ -1,5 +1,7 @@
 from random import randint
 
+from count_min_sketch.count_min_sketch import CountMinSketch
+
 
 def absolute_error(expected, actual):
     return abs(expected - actual)
@@ -11,7 +13,7 @@ def split_number(number, max_splits):
         split = randint(1, number)
         number -= split
         splits.append(split)
-    if max_splits > 0:
+    if number > 0:
         splits.append(number)
     return splits
 
@@ -25,3 +27,22 @@ def generate_data(nb_observations, min_frequency, max_frequency, max_splits=5):
         splits = split_number(number=frequency, max_splits=max_splits)
         data.extend([(observation, split) for split in splits])
     return data, description
+
+
+def benchmark(nb_columns, nb_rows, data, description):
+    count_min_sketch = CountMinSketch(nb_columns=nb_columns, nb_rows=nb_rows)
+    for item, frequency in data:
+        count_min_sketch.add_item(item=item, frequency=frequency)
+    abs_error = list()
+    nb_errors = 0
+    for item, expected_frequency in description.items():
+        actual_frequency = count_min_sketch.retrieve_item_frequency(item=item)
+        nb_errors += actual_frequency != expected_frequency
+        abs_error.append(
+            absolute_error(
+                expected=expected_frequency,
+                actual=actual_frequency
+            )
+        )
+        nb_items = len(description.items())
+    return sum(abs_error) / nb_items, nb_errors / nb_items
--- tests/count_min_sketch/test_benchmark.py
+++ tests/count_min_sketch/test_benchmark.py
@@ -1,5 +1,7 @@
+from collections import defaultdict
+
 import pytest
-from count_min_sketch.benchmark import absolute_error, split_number
+from count_min_sketch.benchmark import absolute_error, generate_data, split_number
 
 
 @pytest.mark.parametrize('expected, actual', [
@@ -22,3 +24,33 @@ def test_split_number(number, max_splits):
     splits = split_number(number=number, max_splits=max_splits)
     assert len(splits) <= max_splits
     assert sum(splits) == number
+    assert 0 not in splits
+
+
+@pytest.mark.parametrize('nb_observations, min_frequency, max_frequency, max_splits', [
+    (4, 1, 10, 3),
+    (100, 100, 200, 10),
+    (1, 1, 1, 1)
+])
+def test_generate_data(nb_observations, min_frequency, max_frequency, max_splits):
+    data, description = generate_data(
+        nb_observations=nb_observations,
+        min_frequency=min_frequency,
+        max_frequency=max_frequency,
+        max_splits=max_splits
+    )
+    # test description
+    assert len(description.items()) == nb_observations
+    for observation, frequency in description.items():
+        assert -1 < observation < nb_observations
+        assert min_frequency <= frequency <= max_frequency
+    # test data
+    data_desc = defaultdict(lambda: {'frequency': 0, 'splits': 0})
+    for observation, frequency in data:
+        data_desc[observation]['frequency'] += frequency
+        data_desc[observation]['splits'] += 1
+    assert len(data_desc.items()) == nb_observations
+    for observation, desc in data_desc.items():
+        assert -1 < observation < nb_observations
+        assert min_frequency <= desc['frequency'] <= max_frequency
+        assert desc['splits'] <= max_splits
