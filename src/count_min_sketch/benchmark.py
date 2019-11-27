1a869d3d5857a43ec6c8705b0aa37936b30135ec
mazghal <aminezghal@vente-privee.com>
2019-11-27 09:18:41 +0100

    add generate_data method

--- src/count_min_sketch/benchmark.py
+++ src/count_min_sketch/benchmark.py
@@ -14,3 +14,14 @@ def split_number(number, max_splits):
     if max_splits > 0:
         splits.append(number)
     return splits
+
+
+def generate_data(nb_observations, min_frequency, max_frequency, max_splits=5):
+    data = list()
+    description = dict()
+    for observation in range(nb_observations):
+        frequency = randint(min_frequency, max_frequency)
+        description[observation] = frequency
+        splits = split_number(number=frequency, max_splits=max_splits)
+        data.extend([(observation, split) for split in splits])
+    return data, description
